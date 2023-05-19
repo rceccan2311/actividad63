4040c1671ccafc35b119c306bf6508cc3b3a11da
rceccan2311 <132548157+rceccan2311@users.noreply.github.com>
2023-05-19 12:01:01 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,12 @@
+let b = 0
+basic.forever(function () {
+    b = 255 - input.lightLevel()
+    led.setBrightness(b)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+})
